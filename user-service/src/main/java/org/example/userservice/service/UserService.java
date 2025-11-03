403c953d24daccaf3a6e50df47f285f706bc770e
CuongNguyen <mac@MacBookPro.lan>
2025-11-03 07:05:31 +0700

    Update: user

---
+++ user-service/src/main/java/org/example/userservice/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.userservice.service;
+
+import org.example.userservice.model.User;
+import org.example.userservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
